1a7cf953ad0140bd95a08452b5b29e985b7617b2
name <email@email.com>
2025-08-22 09:03:42 +0800

    first working api call from openrouter gpt-oss-20b model

--- test1.py
+++ test1.py
@@ -8,29 +8,22 @@ openai.api_key = os.environ["OPENAI_API_KEY"]
 
 print(openai.api_key)
 
-# client = openai.OpenAI()
+from openai import OpenAI
 
-# response = client.chat.completions.create(
-#     model="gpt-3.5-turbo",
-#     messages=[
-#         {"role": "user", "content": "Hi, how are you?"}
-#     ]
-# )
-# print(response.choices[0].message.content)
-# input="Write a wish for me for the coming new year 2026"
-# print(response.output_text)
+client = OpenAI(
+  base_url="https://openrouter.ai/api/v1",
+  api_key=openai.api_key,
+)
 
-# Point the client to DeepSeek's API
-# client = openai.OpenAI(
-#     api_key=openai.api_key,
-#     base_url="https://api.deepseek.com/v1"
-# )
-
-# response = client.chat.completions.create(
-#     model="deepseek-chat",
-#     messages=[
-#         {"role": "user", "content": "What is the meaning of life?"}
-#     ]
-# )
-
-# print(response.choices[0].message.content)
+completion = client.chat.completions.create(
+  extra_headers={},
+  extra_body={},
+  model="openai/gpt-oss-20b:free",
+  messages=[
+    {
+      "role": "user",
+      "content": "What is the meaning of life?"
+    }
+  ]
+)
+print(completion.choices[0].message.content)
